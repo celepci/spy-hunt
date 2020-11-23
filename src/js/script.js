d2fd0156d43d9b6b959ffabaa4e637b6b0011c80
celepci <muhammet.celepci@pixelate.com.tr>
2020-11-23 22:23:50 +0300

    Oyun ekranı eklendi

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,33 @@ $('.js-open-settings-modal').on('click', function () {
 $('.js-open-how-play-modal').on('click', function () {
     $('#howPlay').addClass('active');
 });
+
+var time = $('.game__timer').text();
+var arrTime = time.split(':');
+var min = arrTime[0];
+var sec = arrTime[1];
+console.log(min);
+console.log(sec);
+
+function timeCountdown(min, sec) {
+    var time = $('.game__timer').text();
+    var arrTime = time.split(':');
+    var min = arrTime[0];
+    var sec = arrTime[1];
+
+    if (sec == 00) {
+        min -= 1;
+        sec = 59;
+    } else {
+        sec -= 1;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    $('.game__timer').text(min + ':' + sec);
+
+}
+
+window.setInterval(function () {
+    timeCountdown(min, sec);
+}, 1000)
